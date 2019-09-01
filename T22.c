d4fe8a70ba26ac53543c8139d6c6585f31ce1ee2
Sachini Senanayake <48913793+sachini1997@users.noreply.github.com>
2019-09-01 23:25:50 +0530

    Add files via upload

---
+++ T22.c
@@ -0,0 +1,21 @@
+// T22.c  
+
+#include<stdio.h>
+
+void main()
+{
+
+int b = 10;
+char ch = 's';
+
+printf("Number of boy%c = %d\n",ch,b );
+
+int g = 20;
+
+printf("Number of girl%c = %d\n",ch,g );
+
+printf("Number of student%c = %d\n",ch,b+g );
+	
+
+}
+
